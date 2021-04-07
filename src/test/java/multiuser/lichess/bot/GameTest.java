22eb90589825cc334ba3d45f12b0ed3b3a917c53
SR_Lut3t1um <tobias5@uni-bremen.de>
2021-04-07 15:38:19 +0200

    fixed small code issues

--- src/test/java/multiuser/lichess/bot/GameTest.java
+++ src/test/java/multiuser/lichess/bot/GameTest.java
@@ -4,18 +4,17 @@ import multiuser.lichess.bot.game.Game;
 import multiuser.lichess.bot.game.Move;
 import org.junit.jupiter.api.Test;
 
-public class GameTest {
-	Game game = new Game();
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class GameTest {
+	private final Game game = new Game();
 
 	@Test
-	public void GameLegalMoveFilter() {
-		Move[] moves = new Move[1];
-		moves[0] = new Move("user1", "e2e4");
-		game.move(moves);
-		moves = new Move[3];
+	void GameLegalMoveFilter() {
+		var moves = new Move[3];
 		moves[0] = new Move("user1", "e7e5");
 		moves[1] = new Move("user2", "a7a5");
 		moves[2] = new Move("user3", "a7a5");
-		game.move(moves);
+		assertDoesNotThrow(() -> game.move(moves));
 	}
 }
