8e069c7ba5a0b72fbe0174def9232189e5e80d98
SR_Lut3t1um <tobias5@uni-bremen.de>
2021-04-07 15:42:20 +0200

    fixed small code issues

--- src/test/java/multiuser/lichess/bot/GameTest.java
+++ src/test/java/multiuser/lichess/bot/GameTest.java
@@ -12,9 +12,9 @@ class GameTest {
 	@Test
 	void GameLegalMoveFilter() {
 		var moves = new Move[3];
-		moves[0] = new Move("user1", "e7e5");
-		moves[1] = new Move("user2", "a7a5");
-		moves[2] = new Move("user3", "a7a5");
+		moves[0] = new Move("user1", "e2e4");
+		moves[1] = new Move("user2", "d2a4");
+		moves[2] = new Move("user3", "a2a4");
 		assertDoesNotThrow(() -> game.move(moves));
 	}
 }
